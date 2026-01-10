ee4814f8720e64e8b1e198e0c6834fd7725505d4
준형 <joonhyung@junhyeong-ui-MacBookAir.local>
2026-01-10 20:58:07 +0900

    fe

--- src/main/java/kwonjh0406/joondrive/config/SecurityConfig.java
+++ src/main/java/kwonjh0406/joondrive/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import java.util.List;
 
