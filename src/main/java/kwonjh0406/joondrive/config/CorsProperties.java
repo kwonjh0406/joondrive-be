744429627f70fbf0e4cfcd1c3fcfad52f65e14ce
준형 <joonhyung@junhyeong-ui-MacBookAir.local>
2026-01-10 20:33:53 +0900

    f

---
+++ src/main/java/kwonjh0406/joondrive/config/CorsProperties.java
@@ -0,0 +1,14 @@
+package kwonjh0406.joondrive.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "cors")
+public class CorsProperties {
+    private List<String> allowedOrigins;
+}
